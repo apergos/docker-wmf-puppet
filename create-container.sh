d92acdc293c5b4f81ed480defe1f09b65edba945
Ariel T. Glenn <ariel@wikimedia.org>
2014-03-18 11:07:50 +0200

    make create-container actually create container with different name

--- create-container.sh
+++ create-container.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 if [ -n "$1" ]; then
     tag="$1"
 else
     tag="latest"
 fi
-docker run -d -name puppet -v /sys/fs/selinux:/selinux:ro "ariel/puppet:${tag}"
+docker run -d -name "puppet-${tag}" -v /sys/fs/selinux:/selinux:ro "ariel/puppet:${tag}"
